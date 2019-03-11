4c7e5417e4623afe86e0daf5d5265863b695465d
oltmanns <dgoltmanns@gmail.com>
2019-03-11 14:33:32 -0700

    TTT-6 roolz

--- csharp/ttt_6/TicTacToe/Program.cs
+++ csharp/ttt_6/TicTacToe/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 
 namespace TicTacToe {
-    class Program {
+    public class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello Tic Tac Toe!");
+        }
+
+        public static string ISBN() {
+            return "1234567890123";
         }
     }
 }
--- csharp/ttt_6/TicTacToeTests/SampleTest.cs
+++ csharp/ttt_6/TicTacToeTests/SampleTest.cs
@@ -1,7 +1,8 @@
 
 using NUnit.Framework;
+//using TicTacToe;
 
-namespace TravisTests {
+namespace TicTacToe {
     
     [TestFixture]
     public class SampleTest {
@@ -11,5 +12,18 @@ namespace TravisTests {
             Assert.IsTrue(true);
 //            Assert.Fail("force failure to see CI fail");
         }
+        [Test]
+        public void Verify_Thirteen_Char() {
+            Assert.AreEqual(13, NewClass.ISBN.Length);
+//            Assert.Fail("force failure to see CI fail");
+        }
     }
+
+    public class NewClass{
+        public static string ISBN {
+            get{return "1234567890123";}
+            // return "1234567890123";
+        }
+    }
+
 }
